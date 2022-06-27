0b98992d748c71d21273ee3709184c3f779b7f8f
Shubham Dhage <shubhamdhage930@gmail.com>
2022-06-27 21:27:51 +0530

    binary tree

---
+++ faang/24-BinaryTree.js
@@ -0,0 +1,174 @@
+// Binary Search Tree is a node-based binary tree data structure which has the following properties:
+
+// The left subtree of a node contains only nodes with keys lesser than the node’s key.
+// The right subtree of a node contains only nodes with keys greater than the node’s key.
+// The left and right subtree each must also be a binary search tree.
+
+// Perfect Balanced tree
+// Level 0 -> 2^0 (power) = 1
+// Level 1 -> 2^1 = 2
+// Level 2 -> 2^2 = 4
+// Level 3 -> 2^3 = 8
+// number of nodes = (2 ^ height (levels) - 1)
+// log of nodes = height or steps (log 100 = 2 or 10^2 = 100)
+
+// O(logN) is better than O(n)
+// Insert - O(logN)
+// Lookup - O(logN)
+// Delete - O(logN)
+
+// Unbalanced Binary Tree
+// Insert - O(n)
+// Lookup - O(n)
+// Delete - O(n)
+
+
+
+class Node {
+    /**
+     * 
+     * @param {number} value 
+     */
+    constructor(value) {
+        this.left = null;
+        this.right = null;
+        this.value = value;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+
+    /**
+     * 
+     * @param {number} value 
+     * @returns
+     */
+    insert(value) {
+        const newNode = new Node(value);
+        if (this.root === null) {
+            this.root = newNode;
+        } else {
+            let currentNode = this.root;
+            while (true) {
+                if (value < currentNode.value) {
+                    // Left
+                    if (!currentNode.left) {
+                        currentNode.left = newNode;
+                        return this; // this -> { root: value }
+                    }
+                    currentNode = currentNode.left;
+                } else {
+                    // Right
+                    if (!currentNode.right) {
+                        currentNode.right = newNode;
+                        return this;  // this -> { root: value }
+                    }
+                    currentNode = currentNode.right;
+                }
+            }
+        }
+    }
+
+    // O(n)
+    lookup(value) {
+        if (!this.root) {
+            return false;
+        }
+        let currentNode = this.root;
+        while (currentNode) {
+            if (value < currentNode.value) {
+                currentNode = currentNode.left;
+            } else if (value > currentNode.value) {
+                currentNode = currentNode.right;
+            } else if (currentNode.value === value) {
+                return currentNode;
+            }
+        }
+        return null
+    }
+
+    remove(value) {
+        if (!this.root) {
+            return false;
+        }
+
+        let currentNode = this.root;
+        let parentNode = null;
+
+        while (currentNode) {
+            if (value < currentNode.value) {
+                parentNode = currentNode;
+                currentNode = currentNode.left;
+            } else if (value > currentNode.value) {
+                parentNode = currentNode;
+                currentNode = currentNode.right;
+            } else if (currentNode.value === value) {
+                // We have a match, get to work!
+
+                // Option 1: No right child: 
+                if (currentNode.right === null) {
+                    if (parentNode === null) {
+                        this.root = currentNode.left;
+                    } else {
+
+                        //if parent > current value, make current left child a child of parent
+                        if (currentNode.value < parentNode.value) {
+                            parentNode.left = currentNode.left;
+
+                            //if parent < current value, make left child a right child of parent
+                        } else if (currentNode.value > parentNode.value) {
+                            parentNode.right = currentNode.left;
+                        }
+                    }
+
+                    // Option 2: Right child which doesnt have a left child
+                } else if (currentNode.right.left === null) {
+                    currentNode.right.left = currentNode.left;
+                    if (parentNode === null) {
+                        this.root = currentNode.right;
+                    } else {
+
+                        //if parent > current, make right child of the left the parent
+                        if (currentNode.value < parentNode.value) {
+                            parentNode.left = currentNode.right;
+
+                            //if parent < current, make right child a right child of the parent
+                        } else if (currentNode.value > parentNode.value) {
+                            parentNode.right = currentNode.right;
+                        }
+                    }
+
+                    // Option 3: Right child that has a left child
+                } else {
+
+                    //find the Right child's left most child
+                    let leftmost = currentNode.right.left;
+                    let leftmostParent = currentNode.right;
+                    while (leftmost.left !== null) {
+                        leftmostParent = leftmost;
+                        leftmost = leftmost.left;
+                    }
+
+                    //Parent's left subtree is now leftmost's right subtree
+                    leftmostParent.left = leftmost.right;
+                    leftmost.left = currentNode.left;
+                    leftmost.right = currentNode.right;
+
+                    if (parentNode === null) {
+                        this.root = leftmost;
+                    } else {
+                        if (currentNode.value < parentNode.value) {
+                            parentNode.left = leftmost;
+                        } else if (currentNode.value > parentNode.value) {
+                            parentNode.right = leftmost;
+                        }
+                    }
+                }
+                return true;
+            }
+        }
+    }
+}
